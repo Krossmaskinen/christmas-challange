7b6992d13f2b729f6690a65c7e003983354e7287
Krossmaskinen <bill.kurtson@gmail.com>
2016-12-06 10:26:19 +0100

    Day 1: - update moving pattern - complete part 2 - check for duplicate step on every step, not just movement targets

--- day1/main.js
+++ day1/main.js
@@ -1,8 +1,8 @@
 !function (){
     'use strict';
 
-    // var mapInput = 'R1, R1, R3, R1, R1, L2, R5, L2, R5, R1, R4, L2, R3, L3, R4, L5, R4, R4, R1, L5, L4, R5, R3, L1, R4, R3, L2, L1, R3, L4, R3, L2, R5, R190, R3, R5, L5, L1, R54, L3, L4, L1, R4, R1, R3, L1, L1, R2, L2, R2, R5, L3, R4, R76, L3, R4, R191, R5, R5, L5, L4, L5, L3, R1, R3, R2, L2, L2, L4, L5, L4, R5, R4, R4, R2, R3, R4, L3, L2, R5, R3, L2, L1, R2, L3, R2, L1, L1, R1, L3, R5, L5, L1, L2, R5, R3, L3, R3, R5, R2, R5, R5, L5, L5, R2, L3, L5, L2, L1, R2, R2, L2, R2, L3, L2, R3, L5, R4, L4, L5, R3, L4, R1, R3, R2, R4, L2, L3, R2, L5, R5, R4, L2, R4, L1, L3, L1, L3, R1, R2, R1, L5, R5, R3, L3, L3, L2, R4, R2, L5, L1, L1, L5, L4, L1, L1, R1',
-    var mapInput = 'R8, R4, R4, R8',
+    var mapInput = 'R1, R1, R3, R1, R1, L2, R5, L2, R5, R1, R4, L2, R3, L3, R4, L5, R4, R4, R1, L5, L4, R5, R3, L1, R4, R3, L2, L1, R3, L4, R3, L2, R5, R190, R3, R5, L5, L1, R54, L3, L4, L1, R4, R1, R3, L1, L1, R2, L2, R2, R5, L3, R4, R76, L3, R4, R191, R5, R5, L5, L4, L5, L3, R1, R3, R2, L2, L2, L4, L5, L4, R5, R4, R4, R2, R3, R4, L3, L2, R5, R3, L2, L1, R2, L3, R2, L1, L1, R1, L3, R5, L5, L1, L2, R5, R3, L3, R3, R5, R2, R5, R5, L5, L5, R2, L3, L5, L2, L1, R2, R2, L2, R2, L3, L2, R3, L5, R4, L4, L5, R3, L4, R1, R3, R2, R4, L2, L3, R2, L5, R5, R4, L2, R4, L1, L3, L1, L3, R1, R2, R1, L5, R5, R3, L3, L3, L2, R4, R2, L5, L1, L1, L5, L4, L1, L1, R1',
+    // var mapInput = 'R8, R4, R4, R8',
         mapPath = [],
         currentPos = {
             x: 0,
@@ -58,26 +58,35 @@
 
         for (let path of mapPath) {
             var direction = path.match('[LR]')[0],
+                directionMultiplier,
                 length = parseInt(path.match(/\d+/)[0]);
 
             lookingAt = turnDirections[lookingAt][direction];
+            directionMultiplier = (lookingAt === 'west' || lookingAt === 'south') ? -1 : 1;
 
             if (lookingAt === 'south' || lookingAt === 'west') {
                 length = -length;
             };
 
             if (lookingAt === 'north' || lookingAt ==='south') {
-                currentPos.y += length;
+                for (let i = 0; i < Math.abs(length); i++) {
+                    currentPos.y += directionMultiplier;
+                    checkDoubleVisit();
+                    trail.push({
+                        x: currentPos.x,
+                        y: currentPos.y
+                    });
+                }
             } else {
-                currentPos.x += length;
+                for (let i = 0; i < Math.abs(length); i++) {
+                    currentPos.x += directionMultiplier;
+                    checkDoubleVisit();
+                    trail.push({
+                        x: currentPos.x,
+                        y: currentPos.y
+                    });
+                }
             }
-
-            checkDoubleVisit();
-
-            trail.push({
-                x: currentPos.x,
-                y: currentPos.y
-            });
         }
     }
 
@@ -99,16 +108,6 @@
     var HQDistance = calculateDistance(visitedTwiceLocation);
 
     var indexes = [];
-    var duplicates = trail.filter((item, index) => {
-        if (item.x === visitedTwiceLocation.x && item.y === visitedTwiceLocation.y) {
-            indexes.push(index);
-            return true;
-        }
-        return false;
-    });
-
-    console.log('indexes', indexes);
-    console.log('duplicates', duplicates);
 
     console.log('distance', currentDistance);
     console.log('HQ distance', HQDistance);
